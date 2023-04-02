1dddda2a1134ff9c2310b065d46a66899e3fabb7
palC02 <120254873+palC02@users.noreply.github.com>
2023-04-02 12:29:21 +0530

    Add files via upload

--- Searching/Linear_Search.c
+++ Searching/Linear_Search.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int size;
-    int arr[5];
-    int i,n;
-    printf("Enter The Number of The Elements:\n");
-    scanf("%d",&size);
-    printf("Enter The Data:\n");
-    for(i=0;i<size;i++){
-        scanf("%d",&arr[i]);
-    }
-    printf("Enter The Data You Want to Match:\n");
-    scanf("%d",&n);
-    //int k;
-    for(i=0;i<size;i++){
-        if(arr[i]==n){
-            printf("The Data is Existed\n");
+int Linear_search(int *arr, int size, int x)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == x)
+        {
+            return i;
         }
-        else if(arr[i]!=n){
-            printf("The Data is not Existed\n");
-        }
-        
+        return -1;
     }
-    return 0;
 }
+
+int main()
+{
+    int size;
+    printf("Enter the size of the array:\n");
+    scanf("%d", &size);
+    int *arr = (int *)malloc(size * sizeof(int));
+    printf("Enter the array elements:\n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int x;
+    printf("Enter the value you want to search:\n");
+    scanf("%d", &x);
+
+    int result = Linear_search(arr, size, x);
+    (result == -1)
+        ? printf("Element is not present in array")
+        : printf("Element is present at index %d", result);
+    return 0;
+}    
